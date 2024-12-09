476fd46470da01b5674ce44a8a256e1d460519c1
rudcjs <rudcjs335@gmail.com>
2024-12-09 17:25:03 +0900

    init commit

---
+++ src/test/java/com/example/shop_project/ShopProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shop_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

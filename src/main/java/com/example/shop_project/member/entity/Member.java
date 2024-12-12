99c03e9ead4d9dfe66e3fb720ac38945201a296d
DESKTOP-R12LSJN\KWC <wc5427@naver.com>
2024-12-12 11:44:39 +0900

    Feat: 권한, 멤버십 추가 (일반 사용자 회원가입시 유저, 브론즈 등급 부여)

--- src/main/java/com/example/shop_project/member/entity/Member.java
+++ src/main/java/com/example/shop_project/member/entity/Member.java
@@ -3,12 +3,18 @@ import java.time.LocalDateTime;
 
 import org.hibernate.annotations.processing.Pattern;
 
+import com.example.shop_project.member.Membership;
+import com.example.shop_project.member.Role;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
 import lombok.Data;
 
 @Entity
@@ -46,13 +52,36 @@ public class Member {
 	@Column(name = "updated_at", nullable = false)
     private LocalDateTime updatedAt;
 	
-	// 후에 들어갈 컬럼들
-	// 권한, 등급(멤버십), 프로필 사진 url
+	// 프로필 사진 URL
+    @Column(name = "profile_image_url")
+    private String profileImageUrl;
+
+    // 권한 (스프링 시큐리티와 연동)
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role", nullable = false)
+    private Role role;
+
+    // 회원 등급
+    @Enumerated(EnumType.STRING)
+    @Column(name = "membership")
+    private Membership membership;
 	
 	@PrePersist
     public void prePersist() {
         LocalDateTime now = LocalDateTime.now();
         this.createdAt = now;
         this.updatedAt = now;
+        
+        // 기본값 설정
+        if (this.role == null) 
+            this.role = Role.USER;
+        
+        if (this.membership == null) 
+            this.membership = Membership.BRONZE;
+    }
+	
+	@PreUpdate
+    public void preUpdate() {
+        this.updatedAt = LocalDateTime.now();
     }
 }

c852205a8078572d11b08ce50aebdbc33d6a61d1
gaeunnnnn <gaeunn02@naver.com>
2024-12-13 16:02:19 +0900

    Feat: 카테고리 생성 및 조회 구현

---
+++ src/main/java/com/example/shop_project/category/dto/CategoryResDto.java
@@ -0,0 +1,20 @@
+package com.example.shop_project.category.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class CategoryResDto {
+    private Long categoryId;
+    private String categoryName;
+    private int depth;
+    private List<CategoryResDto> subCategories = new ArrayList<>();
+}

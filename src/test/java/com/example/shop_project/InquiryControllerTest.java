148c07207cd6e3c6eb5488ad7bf5712d3ea93d42
youngchive <102659915+youngchive@users.noreply.github.com>
2024-12-16 22:44:36 +0900

    Test: 테스트 수정

--- src/test/java/com/example/shop_project/InquiryControllerTest.java
+++ src/test/java/com/example/shop_project/InquiryControllerTest.java
@@ -2,8 +2,11 @@ package com.example.shop_project;
 
 import com.example.shop_project.inquiry.entity.Inquiry;
 import com.example.shop_project.inquiry.entity.InquiryRequestDto;
+import com.example.shop_project.inquiry.entity.InquiryType;
 import com.example.shop_project.inquiry.repository.InquiryRepository;
 import com.example.shop_project.inquiry.service.InquiryService;
+import com.example.shop_project.product.entity.Product;
+import com.example.shop_project.product.repository.ProductRepository;
 import jakarta.persistence.EntityNotFoundException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -12,6 +15,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -24,26 +29,39 @@ class InquiryControllerTest {
     @Autowired
     private InquiryRepository inquiryRepository;
 
+    @Autowired
+    private ProductRepository productRepository;
+
     @Test
     @DisplayName("문의 생성")
     void createInquiry() {
         // given
+        Product product = Product.builder()
+                .categoryName("의류")
+                .productName("테스트 상품")
+                .description("테스트 상품 설명")
+                .createdAt(LocalDateTime.now())
+                .updatedAt(LocalDateTime.now())
+                .price(10000)
+                .build();
+        Product savedProduct = productRepository.save(product);
+
         InquiryRequestDto requestDto = InquiryRequestDto.builder()
-                .productId(1L)
                 .nickname("nickname1")
                 .title("title1")
                 .content("content1")
+                .type(InquiryType.SIZE)
                 .build();
 
         // when
-        Inquiry savedInquiry = inquiryService.createInquiry(requestDto);
+        Inquiry savedInquiry = inquiryService.createInquiry(savedProduct.getProductId(), requestDto);
 
         // then
         Inquiry foundInquiry = inquiryRepository.findById(savedInquiry.getId())
                 .orElseThrow(() -> new EntityNotFoundException("문의가 존재하지 않습니다."));
 
         assertEquals(savedInquiry.getId(), foundInquiry.getId());
-        assertEquals(savedInquiry.getProductId(), foundInquiry.getProductId());
+        assertEquals(savedInquiry.getProduct().getProductId(), foundInquiry.getProduct().getProductId());
         assertEquals(savedInquiry.getNickname(), foundInquiry.getNickname());
         assertEquals(savedInquiry.getTitle(), foundInquiry.getTitle());
         assertEquals(savedInquiry.getContent(), foundInquiry.getContent());
@@ -53,12 +71,23 @@ class InquiryControllerTest {
     @DisplayName("문의 조회")
     void getInquiryById() {
         // given
+        Product product = Product.builder()
+                .categoryName("의류")
+                .productName("테스트 상품")
+                .description("테스트 상품 설명")
+                .createdAt(LocalDateTime.now())
+                .updatedAt(LocalDateTime.now())
+                .price(10000)
+                .build();
+        Product savedProduct = productRepository.save(product);
+
         Inquiry inquiry = Inquiry.builder()
-                .productId(1L)
+                .product(savedProduct)
                 .nickname("nickname1")
                 .title("title1")
                 .content("content1")
                 .date(LocalDate.now())
+                .type(InquiryType.SIZE)
                 .build();
         Inquiry savedInquiry = inquiryRepository.save(inquiry);
 
@@ -68,7 +97,7 @@ class InquiryControllerTest {
         // then
         assertNotNull(foundInquiry);
         assertEquals(savedInquiry.getId(), foundInquiry.getId());
-        assertEquals(savedInquiry.getProductId(), foundInquiry.getProductId());
+        assertEquals(savedInquiry.getProduct().getProductId(), foundInquiry.getProduct().getProductId());
         assertEquals(savedInquiry.getNickname(), foundInquiry.getNickname());
         assertEquals(savedInquiry.getTitle(), foundInquiry.getTitle());
         assertEquals(savedInquiry.getContent(), foundInquiry.getContent());
@@ -79,12 +108,23 @@ class InquiryControllerTest {
     @DisplayName("문의 삭제")
     void deleteInquiry() {
         // given
+        Product product = Product.builder()
+                .categoryName("의류")
+                .productName("테스트 상품")
+                .description("테스트 상품 설명")
+                .createdAt(LocalDateTime.now())
+                .updatedAt(LocalDateTime.now())
+                .price(10000)
+                .build();
+        Product savedProduct = productRepository.save(product);
+
         Inquiry inquiry = Inquiry.builder()
-                .productId(1L)
+                .product(savedProduct)
                 .nickname("nickname1")
                 .title("title1")
                 .content("content1")
                 .date(LocalDate.now())
+                .type(InquiryType.SIZE)
                 .build();
         Inquiry savedInquiry = inquiryRepository.save(inquiry);
 

c4c49dfa45274116d0af13bb1cede6b45df51103
DESKTOP-R12LSJN\KWC <wc5427@naver.com>
2024-12-18 23:12:40 +0900

    Feat: 유효성 검증 로직 dto로 이동

--- src/main/java/com/example/shop_project/member/controller/MemberController.java
+++ src/main/java/com/example/shop_project/member/controller/MemberController.java
@@ -44,40 +44,14 @@ public class MemberController {
 	@PostMapping("/join")
 	public String saveMember(@Validated @ModelAttribute MemberRequestDTO memberRequestDTO,
 				            Model model, BindingResult bindingResult) {
-		// 이메일 형식 검사 (@하나 포함, @뒤에 .하나 포함)
-		String emailRegex = "^[a-zA-Z0-9]+@[a-zA-Z0-9]+\\.[a-zA-Z]{2,}$";
-		if (!memberRequestDTO.getEmail().matches(emailRegex)) {
-			model.addAttribute("error", "올바른 이메일 형식이 아닙니다.");
-			return "member/join";
-		}
-		
-		// Base64 인코딩된 비밀번호를 디코딩
-		String decodedPassword = new String(Base64.getDecoder().decode(memberRequestDTO.getPassword()), StandardCharsets.UTF_8);
-		memberRequestDTO.setPassword(decodedPassword);
-		String decodedConfirmPassword = new String(Base64.getDecoder().decode(memberRequestDTO.getConfirmPassword()), StandardCharsets.UTF_8);
-		memberRequestDTO.setConfirmPassword(decodedConfirmPassword);
-	
-		// 패스워드 형식 검사 (최소 8글자, 대소문자, 숫자, 특수문자 최소 하나씩 포함)
-		String passwordRegex = "^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$";
-		if (!memberRequestDTO.getPassword().matches(passwordRegex)) {
-			model.addAttribute("error", "비밀번호는 최소 8글자이며 대소문자, 숫자, 특수문자를 최소 하나씩 포함해야 합니다.");
-			return "member/join";
-		}
-		
-		// 비밀번호 불일치
-		if (!memberRequestDTO.getPassword().equals(memberRequestDTO.getConfirmPassword())) {
-            model.addAttribute("error", "비밀번호가 일치하지 않습니다.");
-            return "member/join";
-        }
-		
 		if (bindingResult.hasErrors()) {
-	        model.addAttribute("error", "모든 필드를 올바르게 입력해주세요.");
-	        return "member/join"; // 다시 회원가입 페이지로
+	        model.addAttribute("error", bindingResult.getFieldError().getDefaultMessage());
+	        return "member/join";
 	    }
 		
 		try {
 			memberService.Join(memberRequestDTO);		
-			return "redirect:/login";
+			return "redirect:/";
 		} catch (IllegalArgumentException e) {
 			model.addAttribute("error", e.getMessage());
             return "member/join";

5e44583b6f8b2db4415037701ad1306b03128996
rudcjs <rudcjs335@gmail.com>
2024-12-23 15:16:30 +0900

    Feat: PayService 상품 재고 조정 로직 추가

--- src/main/java/com/example/shop_project/order/controller/PaymentAPIController.java
+++ src/main/java/com/example/shop_project/order/controller/PaymentAPIController.java
@@ -1,16 +1,16 @@
 package com.example.shop_project.order.controller;
+import com.example.shop_project.order.dto.OrderDetailDto;
 import com.example.shop_project.order.dto.PaymentDto;
+import com.example.shop_project.order.entity.OrderDetail;
 import com.example.shop_project.order.entity.Payment;
 import com.example.shop_project.order.service.PaymentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequestMapping("api/payment")
@@ -22,5 +22,11 @@ public class PaymentAPIController {
     public ResponseEntity<Payment> createPayment(@Validated @RequestBody PaymentDto paymentDto){
         Payment payment = paymentService.createPayment(paymentDto);
         return ResponseEntity.created(URI.create("/")).body(payment);
+    }
+
+    @PatchMapping("/update-product-stock")
+    public ResponseEntity<Void> updateProductStock(List<OrderDetailDto> orderDetailDtoList){
+        paymentService.productStockUpdate(orderDetailDtoList);
+        return ResponseEntity.ok().build();
     }
 }
--- src/main/java/com/example/shop_project/order/service/PaymentService.java
+++ src/main/java/com/example/shop_project/order/service/PaymentService.java
@@ -1,16 +1,25 @@
 package com.example.shop_project.order.service;
 
+import com.example.shop_project.order.dto.OrderDetailDto;
 import com.example.shop_project.order.dto.PaymentDto;
 import com.example.shop_project.order.entity.Order;
+import com.example.shop_project.order.entity.OrderDetail;
 import com.example.shop_project.order.entity.Payment;
 import com.example.shop_project.order.mapper.OrderMapper;
 import com.example.shop_project.order.repository.OrderRepository;
 import com.example.shop_project.order.repository.PaymentRepository;
+import com.example.shop_project.product.dto.ProductResponseDto;
+import com.example.shop_project.product.entity.Product;
+import com.example.shop_project.product.entity.ProductOption;
+import com.example.shop_project.product.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.Objects;
 
 @Service
 public class PaymentService {
@@ -20,6 +29,8 @@ public class PaymentService {
     OrderRepository orderRepository;
     @Autowired
     OrderMapper orderMapper;
+    @Autowired
+    private ProductRepository productRepository;
 
     public Payment createPayment(PaymentDto paymentDto){
         Payment newPayment = orderMapper.toEntity(paymentDto);
@@ -31,5 +42,20 @@ public class PaymentService {
     public Payment getPaymentByOrderNo(Long orderNo){
         Order order = orderRepository.findByOrderNo(orderNo).orElseThrow();
         return paymentRepository.findByOrder(order).orElseThrow();
+    }
+
+    public void productStockUpdate(List<OrderDetailDto> orderDetailDtoList){
+        orderDetailDtoList.forEach(orderDetailDto -> {
+            Product product = productRepository.findById(orderDetailDto.getProductId()).orElseThrow(() -> new NoSuchElementException("상품이 존재하지 않습니다."));
+
+            for(ProductOption option : product.getOptions()){
+                if(option.getSize() == orderDetailDto.getSize() && Objects.equals(option.getColor(), orderDetailDto.getColor())) {
+                    option.setStockQuantity((int) (option.getStockQuantity() - orderDetailDto.getQuantity()));
+                    break;
+                }
+            }
+
+            productRepository.save(product);
+        });
     }
 }

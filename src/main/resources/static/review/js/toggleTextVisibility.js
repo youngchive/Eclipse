0bb0e4e51c664b88153c8464af323691277bdeda
gaeunnnnn <gaeunn02@naver.com>
2024-12-30 11:33:35 +0900

    Fix: 리뷰 텍스트 토글 오류 수정

--- src/main/resources/static/review/js/toggleTextVisibility.js
+++ src/main/resources/static/review/js/toggleTextVisibility.js
@@ -1,9 +1,9 @@
 const reviews = document.querySelectorAll('.content-wrapper');
 
 reviews.forEach(review => {
-    const text = document.querySelector('.review-content');
-    const moreText = document.querySelector('.more-text');
-    const lessText = document.querySelector('.less-text');
+    const text = review.querySelector('.review-content');
+    const moreText = review.querySelector('.more-text');
+    const lessText = review.querySelector('.less-text');
 
     const lineHeight = parseInt(window.getComputedStyle(text).lineHeight, 10);
     const maxHeight = lineHeight * 3;

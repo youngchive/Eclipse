6efd259070420fe0253cf6faa36774f58e167088
gaeunnnnn <gaeunn02@naver.com>
2025-01-02 14:32:10 +0900

    Feat: 카테고리 삭제 시 상품 존재 여부 검사 추가

--- src/main/java/com/example/shop_project/category/exception/CategoryErrorCode.java
+++ src/main/java/com/example/shop_project/category/exception/CategoryErrorCode.java
@@ -8,7 +8,8 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum CategoryErrorCode {
     // 409 Conflict
-    DUPLICATE_RESOURCE(HttpStatus.CONFLICT, "중복된 카테고리명입니다.");
+    DUPLICATE_RESOURCE(HttpStatus.CONFLICT, "중복된 카테고리명입니다."),
+    CONFLICT_WITH_PRODUCTS(HttpStatus.CONFLICT, "해당 카테고리에 상품이 존재하여 삭제할 수 없습니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
